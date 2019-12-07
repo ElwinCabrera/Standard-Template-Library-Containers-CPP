991e5d04f78b890583627ddd64b98b6052632f98
Elwin Cabrera <elwincab0@gmail.com>
2019-12-07 17:42:03 -0500

    added the one away problem and explanation with runtime

--- ds-algos/data-structures/arrays-strings/exercises/main.cpp
+++ ds-algos/data-structures/arrays-strings/exercises/main.cpp
@@ -3,11 +3,13 @@
 #include "urlify.cpp"
 #include "palindrome-permuation.cpp"
 #include "valid-parentheses.cpp"
+#include "one-away.cpp"
 
 int main(int argc, char **argv){
 
-    //cout << isUnique2("abc") << "\n";
-    string s = "fuct coa";
-    cout << isValid("{[]}") << "\n";
+    cout << "1. " << isOneAway("ealp","ple") << "\n";
+    cout << "2. " << isOneAway("pales","pale") << "\n";
+    cout << "3. " << isOneAway("pale","bale") << "\n";
+    cout << "4. " << isOneAway("pale","bake") << "\n";
     return 0;
 }
