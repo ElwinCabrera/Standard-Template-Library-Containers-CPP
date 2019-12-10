56e044259c6ecde7f584452caf530fbb3a40f545
Elwin Cabrera <elwincab0@gmail.com>
2019-12-10 15:57:58 -0500

    added explanations to zero-matrix problem

--- ds-algos/data-structures/arrays-strings/exercises/zero-matrix.cpp
+++ ds-algos/data-structures/arrays-strings/exercises/zero-matrix.cpp
@@ -1,6 +1,15 @@
 #include <vector>
 using std::vector;
 /*
+Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0
+*/
+
+/*
+In this approach we first check if we need to set the top row or left column to zero at the end we do this by keeping too boolean variables one for the top row and one for the left column and if there is a zero in any of these sections we set the corresponding boolean variable to true. 
+Then we iterate over the rest of the matrix (not including the top row or left most column) and everytime we see a zero we set the corresponding column at the top row to zero and the coresponding row for the left column to zero.
+
+Runtime: O(n^2) - where n is the number of integers in the matrix, we cant do better because we need to visit every element in the matrix.
+Space: O(1) - no extra space is used.
 
 */
 void zeroMatrix(vector<vector<int>> &matrix){
