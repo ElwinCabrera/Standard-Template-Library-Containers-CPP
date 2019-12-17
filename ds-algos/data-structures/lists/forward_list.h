695c1d2ee42e05fb1093b881a89c5e11149c6615
Elwin Cabrera <elwincab0@gmail.com>
2019-12-16 23:07:53 -0500

    figured out the iterator thing and finishing the forward list implementation

--- ds-algos/data-structures/lists/forward_list.h
+++ ds-algos/data-structures/lists/forward_list.h
@@ -3,21 +3,18 @@
 
 #include <stdexcept>
 #include <iostream>
-#include <string.h>
+#include <cassert>      // assert
 #include <algorithm>
 #include <climits>
 #include <math.h>
-#include <new>
-#include <forward_list>
 
-template< class Type, class UnqualifiedType  > class Forward_Iterator;
+template< class Type, class UnqualifiedType > class Forward_Iterator;
 template<class Type, class UnqualifiedType  > class Forward_List;
 
 template <class Type, class UnqualifiedType = std::remove_cv_t<Type>> 
 class Node 
 {
-private:
-    
+public:
     Node(): data(0), next_node(0) {}
     Node(const UnqualifiedType &value, Node<Type> *ptr= nullptr): data(value), next_node(0) {}
     
@@ -41,44 +38,50 @@ public:
     Forward_Iterator(): nodeItr(nullptr) {}   
     Forward_Iterator(Node<Type>* nItr): nodeItr(nItr) {}
     // Default Copy/Move Are Fine.
-    Forward_Iterator(Forward_Iterator &it)  = default;
+    Forward_Iterator(const Forward_Iterator &it)  = default;
     Forward_Iterator(Forward_Iterator &&it) = default;
     // Default Destructor fine.
     ~Forward_Iterator() = default;
 
-    reference       operator*()             {return this->nodeItr->next_node->data;}
-    const reference operator*()       const {return this->nodeItr->next_node->data;}
-    pointer         operator->()            {return this->nodeItr; }
-    const pointer   operator->()      const {return this->nodeItr;}
+    Forward_Iterator& operator=(const Forward_Iterator &other) {
+        Forward_Iterator tmpItr(other);
+        tmpItr.swap(*this);
+        return *this;
+    }
+
+    Node<Type>*       operator*()             {return this->nodeItr;}
+    const Node<Type>* operator*()       const {return this->nodeItr;}
+    Node<Type>*         operator->()            {return this->nodeItr; }
+    const Node<Type>*   operator->()      const {return this->nodeItr;}
 
 
     Forward_Iterator& operator++() { // Pre-increment
         assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
-        this->nodeItr = this->nodeItr->next_node->next_node;
+        this->nodeItr = this->nodeItr->next_node;
         return *this;
     }
     // Forward_Iterator& operator--() { // Pre-decrement
     //     assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
-    //     this->nodeItr = this->nodeItr->prev_node->prev_node;
+    //     this->nodeItr = this->nodeItr->prev_node;
     //     return *this;
     // }
     Forward_Iterator  operator++(int){ // Post-increment
         assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
         Forward_Iterator origItr(*this);
-        this->nodeItr = this->nodeItr->next_node->next_node;
+        this->nodeItr = this->nodeItr->next_node;
         return origItr;
     }
     // Forward_Iterator  operator--(int){ // Post-increment
     //     assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
     //     Forward_Iterator origItr(*this);
-    //     nodeItr = nodeItr->prev_node->prev_node;
+    //     nodeItr = nodeItr->prev_node;
     //     return origItr;
     // }
 
     Forward_Iterator& operator+=(int n)  {
         assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
         while (n && this->nodeItr){
-            this->nodeItr = this->nodeItr->next_node->next_node;
+            this->nodeItr = this->nodeItr->next_node;
             --n;
         }
         return *this;
@@ -86,7 +89,7 @@ public:
     // Forward_Iterator& operator-=(int n){
     //     assert(this->nodeItr != nullptr && "Out-of-bounds iterator increment!");
     //     while (n && this->nodeItr){
-    //         this->nodeItr = this->nodeItr->prev_node->prev_node;
+    //         this->nodeItr = this->nodeItr->prev_node;
     //         --n;
     //     }
     //     return *this;
@@ -125,6 +128,11 @@ public:
 
     operator Forward_Iterator<const Type>() const { return Forward_Iterator<const Type>(this->nodeItr); }
 
+    void swap(Forward_Iterator<Type>& other) noexcept{
+        using std::swap;
+        swap(this->nodeItr, other.nodeItr);
+    }
+
 private:
     Node<Type>* nodeItr;
 };
@@ -134,14 +142,13 @@ template<class Type, class UnqualifiedType = std::remove_cv_t<Type> >
 class Forward_List{
 public:
 
-    typedef class Forward_Iterator<Type> iterator;
-    typedef class Forward_Iterator<const Type> const_iterator;
+    typedef Forward_Iterator<UnqualifiedType> iterator;
+    typedef const  Forward_Iterator<UnqualifiedType> const_iterator;
     
     Forward_List(): first_node(new Node<Type>()), last_node(new Node<Type>()) ,size(0) {this->first_node->next_node = this->last_node;}
 
     Forward_List(unsigned count, const UnqualifiedType & value)
     : first_node(new Node<Type>()), last_node(new Node<Type>()), size(count){
-        this->first_node->next_node = this->last_node;
         Node<Type> *node_ptr = this->first_node;
         while(count ) {
             Node<Type> *new_node = new Node<Type>(value);
@@ -154,7 +161,6 @@ public:
 
     Forward_List(unsigned count)
     : first_node(new Node<Type>()), last_node(new Node<Type>()), size(count) {
-        this->first_node->next_node = this->last_node;
         Node<Type> *node_ptr = this->first_node;
         while(count ) {
             Node<Type> *new_node = new Node<Type>();
@@ -166,13 +172,13 @@ public:
 
     }
 
-    template<class InputIt>
-    Forward_List(InputIt first, InputIt last)
+    
+    Forward_List(Forward_Iterator<Type> first, Forward_Iterator<Type> last)
     : first_node(new Node<Type>()), last_node(new Node<Type>()), size(0){
         this->first_node->next_node = this->last_node;
         Node<Type> *node_ptr = this->first_node;
         while(first != last){
-            Node<Type> *new_node = new Node<Type>(*first);
+            Node<Type> *new_node = new Node<Type>(first->data);
             node_ptr->next_node = new_node;
             node_ptr = node_ptr->next_node;
             ++size;
@@ -185,15 +191,17 @@ public:
         Forward_List::iterator it (other.begin());
         Node<Type> *node_ptr = this->first_node;
         while (it != other.end()){
-            Node<Type> *new_node = new Node<Type>(*first);
+            Node<Type> *new_node = new Node<Type>(*it);
             node_ptr->next_node = new_node;
             node_ptr = node_ptr->next_node;
+            it++;
         }
         node_ptr->next_node = this->last_node;
     }
     Forward_List(const Forward_List &&other)
-    : first_node(new Node<Type>()), last_node(new Node<Type>()), size(other.size()){
-        
+    : size(other.size()){
+        this->first_node = other.first_node;
+        other.first_node = nullptr;
     }
     ~Forward_List(){
         if(this->first_node){
@@ -207,7 +215,7 @@ public:
 
         }
     }
-    Forward_List<UnqualifiedType >& operator=(const Forward_List<Type >& other){
+    Forward_List<UnqualifiedType >& operator=(const Forward_List<Type>& other){
         Forward_List<Type> tmp(other);
         tmp.swap(*this);
         return *this;
@@ -217,20 +225,27 @@ public:
 
     }
 
-    void assign(unsigned count, const Type& value){
-        this->resize(count);
-
-    }
+    void assign(unsigned count, const UnqualifiedType& value){ this->resize(count, value); }
 
     
 
     //element access
-    UnqualifiedType& front() {return  &first_node->value;}
-    const UnqualifiedType& front() const {return  &first_node->value;}
+    UnqualifiedType& front() {return  this->begin()->data;}
+    const UnqualifiedType& front() const {return  this->begin()->data;}
     
-    typename Forward_List<Type>::iterator begin() {}
+    // iterators
+    iterator before_begin() noexcept {return iterator(this->first_node);}
+    const iterator before_begin() const noexcept {return const_iterator(this->first_node);}
+    const iterator cbefore_begin() const noexcept {return const_iterator(this->first_node);}
     
-
+    iterator begin() noexcept {return iterator(first_node)+1;}
+    const_iterator begin() const noexcept{return const_iterator(first_node)+1;}
+    const_iterator cbegin() const noexcept{return const_iterator(first_node)+1;}
+    
+    iterator end() noexcept{return iterator(last_node);}
+    const_iterator end() const noexcept{return const_iterator(last_node);}
+    const_iterator cend() const noexcept{return const_iterator(last_node);}
+    
     //capacity
     unsigned empty() {return size == 0;}
     unsigned max_size(){return UINT_MAX;}
@@ -246,31 +261,130 @@ public:
                 delete trash;
             }
         }
+        this->size = 0;
         first_node = new Node<Type>();
         last_node = new Node<Type>();
         first_node->next_node = last_node;
     }
     iterator insert_after(const_iterator pos, const UnqualifiedType &value) {
-        
+        iterator p = pos;
+        iterator next = pos + 1;
+        Node<Type> *node = new Node<Type>(value);
+        if(next != this->end()){
+            p->next_node = node;
+            node->next_node = *next;
+        } else {
+            p->next_node = node;
+            node->next_node = last_node;
+        }
+        ++this->size;
+        return next;
     }
-    iterator insert_after(const_iterator pos, const UnqualifiedType &&value);
-    iterator insert_after(const_iterator pos, unsigned count, const UnqualifiedType &value);
+    iterator insert_after(const_iterator pos, const UnqualifiedType &&value){
+        const UnqualifiedType v = std::move(value);
+        return insert_after(pos, v);
+    }
+    iterator insert_after(const_iterator pos, unsigned count, const UnqualifiedType &value){
+        for(const_iterator it = pos; it != pos + count; ++it) insert_after(it, value);
+    }
+
     template<class InputIt>
     iterator insert_after(const_iterator pos, InputIt first, InputIt last);
     // template< class... Args >
     // iterator emplace_after( const_iterator pos, Args&&... args );
-    iterator erase_after(iterator pos);
-    iterator erase_after(iterator first, iterator last);
-    void push_front(const UnqualifiedType & value);
-    void push_front(UnqualifiedType && value);
+    iterator erase_after(const_iterator pos){
+        if(pos == this->end() || (pos +1) == this->end() || *pos == nullptr) return this->end();
+        iterator curr = pos;
+        iterator trash = pos + 1;
+        iterator next = trash->next_node;
+        curr->next_node = *next;
+        if(*trash) {
+            trash->next_node = nullptr;
+            delete *trash;
+            --this->size;
+        }
+        return next;
+    }
+    iterator erase_after(const_iterator first, const_iterator last){
+        iterator it = first + 1;
+        iterator firstIt = first;
+        iterator lastIt = last;
+        firstIt->next_node = *lastIt;  
+             
+        while(it != last){
+            iterator trash = it;
+            iterator next = trash->next_node;
+            if(*trash) {
+                trash->next_node = nullptr;
+                delete *trash;
+                --this->size;
+            }
+            it = next;
+        }
+        firstIt->next_node =  *lastIt;
+        return last;
+    }
+    
+
+    void push_front(const UnqualifiedType & value){
+        Node<Type> *node = new Node<Type>(value);
+        node->next_node = this->first_node->next_node;
+        this->first_node->next_node = node;
+        ++this->size;
+    }
+
+    void push_front(UnqualifiedType && value){
+        UnqualifiedType v= std::move(value);
+        push_front(v);
+    }
+
     // template< class... Args >
     // void emplace_front( Args&&... args );
     // template< class... Args >
     // reference emplace_front( Args&&... args );
-    void pop_front();
-    void resize(unsigned count);
-    void resize(unsigned count, const UnqualifiedType & value);
-    void swap(Forward_List<Type>& other);
+    void pop_front(){
+        Node<Type> *trash = this->first_node->next_node;
+        if(trash && trash != this->last_node){
+            this->first_node->next_node = trash->next_node;
+            delete trash;
+            --this->size;
+        }
+    }
+    void resize(unsigned count) {
+        
+        iterator it = this->begin();
+        unsigned c = 0;
+        if(count > this->size){
+            c = count - this->size;
+            while(it + 1 != this->end()) ++it;
+            while(c--) this->insert_after(it,0);
+        } else if (count < this->size){
+            c = count;
+            while(--c) ++it;
+            this->erase_after(it, this->end());
+        }
+        this->size = count;
+    }
+    void resize(unsigned count, const UnqualifiedType & value){
+        
+        iterator it = this->begin();
+        unsigned c = 0;
+        if(count > this->size){
+            c = count - this->size;
+            while(it + 1 != this->end()) ++it;
+            while(c--) this->insert_after(it,value);
+        } else if (count < this->size){
+            c = count;
+            while(--c) ++it;
+            this->erase_after(it, this->end());
+        }
+        this->size = count;
+    }
+    void swap(Forward_List<Type>& other) noexcept{
+        using std::swap;
+        swap(this->size, other.size);
+        swap(this->first_node, other.first_node);
+    }
     //void swap(Forward_List& other) noexcept;
 
     //operations
@@ -286,8 +400,34 @@ public:
     void splice_after(iterator pos, Forward_List<Type> &&other, iterator it);
     void splice_after(iterator pos, Forward_List<Type> &other, iterator first, iterator last);
     void splice_after(iterator pos, Forward_List<Type> &&other, iterator first, iterator last);
-    unsigned remove(const UnqualifiedType & value);
-    void reverse() noexcept;
+
+    unsigned remove(const UnqualifiedType & value){
+        unsigned count = 0;
+        if((this->before_begin() +1)->data == value && (this->before_begin() +1) != this->end()) { erase_after(this->before_begin()); ++count; }
+        iterator it = this->begin();
+        while(it +1  != this->end()){
+            if((it+1)->data == value){ erase_after(it); ++count; }
+            else ++it; 
+        }
+        this->size -= count;
+        return count;
+    }
+    void reverse() noexcept{
+        Node<Type> *before_begin = nullptr;
+        Node<Type> *begin = this->first_node;
+        Node<Type> *after_begin = begin->next_node;
+
+        while(begin ){
+            begin->next_node = before_begin;
+            before_begin = begin;
+            begin = after_begin;
+
+            if(after_begin ) after_begin = after_begin->next_node;
+        }
+        Node<Type> *tmp = this->first_node;
+        this->first_node =  last_node;
+        last_node = tmp;
+    }
     unsigned unique();
     void sort();
     // template< class Compare >
@@ -295,21 +435,35 @@ public:
 
 
     //non-member operators
-    friend bool operator==(const Forward_List<Type> &l1, const Forward_List<Type> &l2) { }
-    friend bool operator!=(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
+    friend bool operator==(const Forward_List<Type> &l1, const Forward_List<Type> &l2) { 
+        auto it1 = l1.begin();
+        auto it2 = l2.begin();
+        while(it1++ != l1.end() && it2++ != l2.end()) if(it1->data != it2->data) return false;
+        return true;
+    }
+    friend bool operator!=(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {
+        auto it1 = l1.begin();
+        auto it2 = l2.begin();
+        while(it1++ != l1.end() && it2++ != l2.end()) if(it1->data != it2->data) return true;
+        return false;
+    }
     friend bool operator<(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
     friend bool operator<=(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
     friend bool operator>(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
     friend bool operator>=(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
     //friend Forward_List<Type> operator+(const Forward_List<Type> &l1, const Forward_List<Type> &l2) {}
+
+    
     friend std::ostream& operator<<(std::ostream &os, const Forward_List<Type> &l) {
-        s.put('[');
-        char arrow[3] = {'\0', ' ', '\0'};
-        for (const auto& e : v) {
-            s << comma << e;
-            arrow[0] = '->';
+        
+        os.put('[');
+        char arrow[2] = {'\0', ' '};
+        for (auto it = l.begin(); it != l.end(); ++it) {
+            os << arrow << it->data;
+            arrow[0] = '-';
+            arrow[1] = '>';
         }
-        return s << ']';
+        return os << ']';
     }
     friend std::istream& operator>>(std::istream &is, const Forward_List<Type> &l) {}
 
--- ds-algos/data-structures/lists/main.cpp
+++ ds-algos/data-structures/lists/main.cpp
@@ -4,6 +4,23 @@
 int main(int argc, char **argv){
 
     Forward_List<int> f_list;
-    Forward_List<int>::iterator it(f_list.begin());
+    //for (int i = 3; i >=1; --i) f_list.push_front(i);
+    f_list.push_front(0);
+    f_list.push_front(3);
+    f_list.push_front(1);
+    f_list.push_front(2);
+    f_list.push_front(1);
+    f_list.push_front(0);
+    f_list.push_front(3);
+    f_list.push_front(2);
+    std::cout << "The forward_list holds: " << f_list << "\n";
+    //f_list.resize(4, 1);
+    std::cout << "After resize up to 5: " << f_list << '\n';
+    //f_list.resize(2);
+    std::cout << "After resize down to 2: " << f_list << '\n';
+    f_list.remove(2);
+    std::cout << "After remove : " << f_list << '\n';
+    f_list.reverse();
+    std::cout << "After reverse: " << f_list << '\n';
     return 0;
 }
